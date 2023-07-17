ed87f312f14cab21122742190cbb5e2f9304eadc
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2023-07-16 22:02:01 -0300

    Add files via upload

---
+++ restore_vm_rsync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Restore the VMs that were sent with the backup script
+
+ipBakupServer=0.0.0.0
+portBakupServer=22
+userBakupServer=user
+folderBakupServer=/backup/folder
+VmFolder=/var/lib/libvirt/images #Default Libvirt folder
+
+rsync -ave "ssh -p $portBakupServer" --mkpath --sparse $userBakupServer@$ipBakupServer:$folderBakupServer/* $vmFolder/restore
+
+for vm in $vmFolder/restore/* # Temporary folder to restore all the VMs
+do
+        vm=${vm##*/} #Ignore all the path of the folders
+	mv $vmFolder/restore/$vm/$vm.qcow2 $vmFolder
+        virsh define $vmFolder/restore/$vm/$vm.xml
+done
+rm -r $vmFolder/restore
